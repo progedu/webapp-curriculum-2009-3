c41ca60573bf79eb9c459c0a7e9abddd19ed0e6b
murosan <masashi_16n1200013@nnn.ed.jp>
2017-04-17 14:41:23 +0900

    挑戦の上級

--- src/test/scala-2.11/SctackSpec.scala
+++ src/test/scala-2.11/SctackSpec.scala
@@ -1,6 +1,29 @@
 import org.scalatest._
 
-class NumberFormatterSpec extends FlatSpec with DiagrammedAssertions {
+class SctackSpec extends FlatSpec with DiagrammedAssertions {
+
+  it should "EmptyStackが作れる" in {
+    assert(Stack() === EmptyStack)
+    assert(Stack().isEmpty === true)
+  }
+
+  it should "NonEmptyStackが作れる" in {
+    val NonEmpStack = Stack().push(1)
+    assert(NonEmpStack.isEmpty === false)
+  }
+
+  "pop,pushメソッド" should "値を追加し、順番通りに取り出せる" in {
+    val EmpStack = Stack()
+    val NonEmpStack = EmpStack.push(1).push(2)
+    assert(NonEmpStack.pop._1 === 2)
+  }
+
+  it should "popする値がない時はエラーが投げられる" in {
+    val EmpStack = Stack()
+    intercept[IllegalArgumentException] {
+      EmpStack.pop
+    }
+  }
 
 }
 
