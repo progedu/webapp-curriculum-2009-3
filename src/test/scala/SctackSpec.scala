1add8c8ac4067241451fbcda8d02ff624e9ff0f8
syo <M.youta.kwg@gmail.com>
2019-05-21 01:39:12 +0900

    Stackのテストを追加

--- src/test/scala/SctackSpec.scala
+++ src/test/scala/SctackSpec.scala
@@ -1,6 +1,24 @@
 import org.scalatest._
 
-class NumberFormatterSpec extends FlatSpec with DiagrammedAssertions {
+class StackSpec extends FlatSpec with DiagrammedAssertions {
+  
+  it should "isEmptyは空のStackはtrue,空でないStackはfalseを返す" in {
+    assert(EmptyStack.isEmpty)
+    assert(!EmptyStack.push(1).isEmpty)
+  }
 
+  it should "LIFOの動作をする" in {
+    val stack = Stack()
+    val first = stack.push(1)
+    val second = first.push(2)
+    assert(second.pop._1 == 2)
+    assert(second.pop._2.pop._1 == 1)
+  }
+
+  it should "空のStackからpopしようとすると実行時例外が投げられる" in {
+    intercept[IllegalArgumentException] {
+      EmptyStack.pop
+    }
+  }
 }
 
