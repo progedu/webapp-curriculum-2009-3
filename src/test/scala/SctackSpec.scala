f88df9c43be852e0ef9d9a43e0f790b873c4cc4f
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-05-26 19:20:32 +0900

    SctackSpec実装

--- src/test/scala/SctackSpec.scala
+++ src/test/scala/SctackSpec.scala
@@ -1,6 +1,29 @@
 import org.scalatest._
 
-class NumberFormatterSpec extends FlatSpec with DiagrammedAssertions {
+class SctackSpec extends FlatSpec with DiagrammedAssertions {
+  "スタック" should "逆順取得できるか?" in {
+    val stack = Stack()
+    val stackPushed1 = stack.push(1)
+    val stackPushed2 = stackPushed1.push(2)
+    val (poped2, stackPoped2) = stackPushed2.pop
+    val (poped1, stackPoped1) = stackPoped2.pop
+    assert(poped2 === 2)
+    assert(poped1 === 1)
+  }
 
+  "isEmpty関数" should "本当に空か?" in {
+    val stack = Stack()
+    assert(stack.isEmpty === true)
+    val stackPushed1 = stack.push(1)
+    assert(stackPushed1.isEmpty === false)
+    val (_, stackPoped1) = stackPushed1.pop
+    assert(stackPoped1.isEmpty === true)
+  }
+
+  "空のスタック" should "popすると例外発生するか?" in {
+    intercept[IllegalArgumentException] {
+      Stack().pop
+    }
+  }
 }
 
